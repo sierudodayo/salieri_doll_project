8fd2d27d6a6c2f83d65ffe33d2920d04780acd54
sierudodayo <curomu0000@gmail.com>
2025-03-12 22:30:23 +0900

    upload_system

--- smart_uploder_beta/assets/js/uploader.js
+++ smart_uploder_beta/assets/js/uploader.js
@@ -37,26 +37,81 @@ inputImage.addEventListener("change", () => {
 });
 
 /* Web Bluetooth API */
-async function connctionToBluetooth() {
+// async function connctionToBluetooth() {
+//     try {
+//         const SERVICE_UUID = "b65ac29b-346b-4c47-a64c-c6785bb7800b";
+//         const device = await navigator.bluetooth.requestDevice({
+//             acceptAllDevices: true,
+//             optionalServices: [SERVICE_UUID]
+//         });
+//         connectionStatus = true;
+//         console.log('選択されたデバイス:', device.name);
+
+//         const server = await device.gatt.connect();
+//         console.log("connected");
+//         const services = await server.getPrimaryServices();
+//         console.log("services", services);
+
+//         if (server) {
+//             console.log( "connect true;");
+//             console.log(server);
+//         } else {
+//             console.log( "connect false;");
+//         }
+//     } catch(error) {
+//         console.log('デバイスの選択に失敗しました:', error);
+//     }
+// }
+
+// TEST web bluetooth
+const SERVICE_UUID = "b65ac29b-346b-4c47-a64c-c6785bb7800b";
+const CHARACTERISTIC_UUID = "9b3f623b-9c48-4fca-840e-b43e6b4fc8e5";
+let characteristic;
+
+async function sendImage() {
+    const fileInput = document.getElementById('file_put_btn');
+    
+    if (!fileInput.files.length) {
+        console.log("not read Image file !!");
+    }
+
+    const file = fileInput.files[0];
+    const imageData = await file.arrayBuffer();
+    const uint8Data = new Uint8Array(imageData);
+    const totalLength = uint8Data.length;
+
+    console.log(`Image file loaded : ${totalLength} bytes.`);
+
     try {
         const device = await navigator.bluetooth.requestDevice({
-            acceptAllDevices: true
+            filters: [{ name: "SmartPhoneForDolls"}],
+            optionalServices: [SERVICE_UUID]
         });
-        connectionStatus = true;
-        console.log('選択されたデバイス:', device.name);
-
         const server = await device.gatt.connect();
+        const service = await server.getPrimaryService(SERVICE_UUID);
+        characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
 
-        if (server) {
-            console.log( "connect true;");
-            console.log(server);
-        } else {
-            console.log( "connect false;");
+        console.log("connected to ESP32");
+
+        const chunkSize = 244;
+        for (let i = 0; i < totalLength; i+= chunkSize) {
+            const chunk = uint8Data.slice(i, i + chunkSize);
+            await characteristic.writeValue(chunk);
+            console.log(`Status sent ${i + chunk.length} / ${totalLength} bytes`);
+            await new Promise(resoleve => setTimeout(resoleve, 10));
         }
-    } catch(error) {
-        console.log('デバイスの選択に失敗しました:', error);
+
+        // fin data
+        // let endStr = Uint8Array.from(0x00);
+        // console.log(endStr);
+        // await characteristic.writeValue(endStr);
+        console.log("completed send image file.");
+    } catch (e) {
+        console.log("error!!");
+        console.log(e);
     }
 }
+
 
 function checkConnectStatus() {
     if (!connectionStatus) {
